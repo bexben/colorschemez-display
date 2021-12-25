5d5431d79b0f52d748e67f6e9ed2708e2ff3de7f
LiquidPropulsion <ufliquidpropulsion@gmail.com>
2021-12-25 15:45:38 -0500

    Delete tokens.h

--- tokens.h
+++
@@ -1,8 +0,0 @@
-const char* ssid = "ATT4rXYQXi"; // enter SSID
-const char* pass = ""; // enter password
-
-#define CONSUMER_KEY        "UA4GaKchqiGNb41MMfIJaonHV" // aka api key
-#define CONSUMER_KEY_SECRET "NVrjystaqsoNvcsmJHboJXCaNClCpTH3pCOLJLvYdyCcmvaoMb"
-#define ACCESS_TOKEN        "838924627434962944-8T9bKQJ56t8RoYdqz3PHnAw3t6JuWNN"
-#define ACCESS_TOKEN_SECRET "JODv28xZ8LmilN5Ni6PzSMguEzub5k0AJxMB6PooUtbCv"
-#define BEARER_TOKEN        "AAAAAAAAAAAAAAAAAAAAAE4dWQEAAAAA9Q%2Fzrko%2F5NI20LiC%2FGP2%2FpUAEPI%3DmrsIOMQ6b5gs3FQmtbjwhoxpau2snll8co7fRmibLkpFMXEzth"
